2d20e9b6e96c0e1a4eb8799d30fb8db1fa5f9c3d
Ivan Lavryk <vanonext.nl@gmail.com>
2024-12-10 17:58:33 +0200

    user.email=vanonext.nl@gmail.com

---
+++ tests/api/test_http.py
@@ -0,0 +1,13 @@
+import pytest
+import requests
+
+
+@pytest.mark.http
+def test_first_request():
+    r = requests.get('https://api.github.com/zen')
+    print(f"Response is {r.text}")
+
+@pytest.mark.http
+def test_second_request():
+    r = requests.get('https://api.github.com/users/defunkt')
+    print(f"Response Body is {r.text}")
