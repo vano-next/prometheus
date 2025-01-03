75e43f7d3170d791ccca58aa00e3f7a06fa1743f
Ivan Lavryk <vanonext.nl@gmail.com>
2025-01-03 23:52:28 +0200

    Проєктне завдання 6 частина 2 видалено секрети

--- tests/ui/test_novaposhta_search.py
+++ tests/ui/test_novaposhta_search.py
@@ -1,14 +1,10 @@
 import pytest
-import os
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.firefox.service import Service
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from webdriver_manager.firefox import GeckoDriverManager
-
-# GitHub токен
-os.environ['GH_TOKEN'] = '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'
 
 @pytest.mark.ui
 def test_search_parcel():
